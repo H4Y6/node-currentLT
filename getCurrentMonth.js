765ee9920281da2c3239b0c31cf25020fde1c70b
H4Y6 <h4y6@i.ua>
2023-01-03 17:55:30 +0200

    modified:   getCurrentMonth.js

--- getCurrentMonth.js
+++ getCurrentMonth.js
@@ -1,5 +1,40 @@
 const getCurrentMonth = () => {
-  const date = new Date();
-  return date.getMonth() + 1;
+  const date = new Date().getMonth() + 11;
+  if (date === 0) {
+    return "january";
+  }
+  if (date === 1) {
+    return "february";
+  }
+  if (date === 2) {
+    return "march";
+  }
+  if (date === 3) {
+    return "april";
+  }
+  if (date === 4) {
+    return "may";
+  }
+  if (date === 5) {
+    return "june";
+  }
+  if (date === 6) {
+    return "july";
+  }
+  if (date === 7) {
+    return "august";
+  }
+  if (date === 8) {
+    return "september";
+  }
+  if (date === 9) {
+    return "october";
+  }
+  if (date === 10) {
+    return "november";
+  }
+  if (date === 11) {
+    return "december";
+  }
 };
 module.exports = getCurrentMonth;
