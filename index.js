a85e19c583d6018f12cf8c3cb3fa4d9de7cf645e
H4Y6 <h4y6@i.ua>
2023-01-03 14:50:57 +0200

    modified:   index.js

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const date = new Date();
 const today = date.toString().slice(0, 25);
 console.log(today);
 
-// const currentMonth = date.getMonth() + 1;
-// console.log(currentMonth);
+const currentMonth = date.getMonth() + 1;
+console.log(currentMonth);
